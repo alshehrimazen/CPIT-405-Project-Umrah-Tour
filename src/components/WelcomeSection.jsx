1d34469dbd41712069fe2c7c83a8a8b5ca7b6023
mazen alshehri <mazen4sa@gmail.com>
2025-04-17 13:28:30 +0300

    Starting the project

---
+++ src/components/WelcomeSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./WelcomeSection.css";
+
+const WelcomeSection = () => {
+  return (
+    <div className="welcome-container">
+      <h1>Welcome to Umrah Tour</h1>
+      <button>Create Your Tour</button>
+    </div>
+  );
+};
+
+export default WelcomeSection;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import WelcomeSection from "../components/WelcomeSection";
+
+const Home = () => {
+  return <WelcomeSection />;
+};
+
+export default Home;
